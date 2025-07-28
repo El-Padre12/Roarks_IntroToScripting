e148a926720cbd50f957d9338350404d771390db
AngelC <noel0402.ac@gmail.com>
2025-07-28 15:39:39 -0500

    completed campus bookstore inventory lab using dictionaries

---
+++ module3/labs/book_inventory.py
@@ -0,0 +1,32 @@
+# Angel Chavez
+# Jul 28th, 2025
+# Python 3.13.5
+# book store inventory manager using dictionaries
+
+inventory = {
+    'Intro to Python': 8,
+    'Data Structures & Algorithms': 25,
+    'Web Development': 14,
+    'Linear Algebra': 3
+}
+
+print('Current Inventory\n')
+for item, quantity in inventory.items():
+    print(f'- {item}: {quantity} in stock')
+
+while True:
+    purchased = input('Enter the title of the book you would like to purchase(enter "q" to quit): ')
+
+    if purchased in inventory:
+        print(f'\n"{purchased}" purchased successfully')
+        inventory[purchased] -= 1
+    elif purchased == 'q':
+        print('Thank you for shopping goodbye! ')
+        break
+    else:
+        print(f'\nThat book "{purchased}" does not seem to be in our inventory')
+        print('Ensure you typed the title correctly(case-sensitive)')
+
+    print('\nUpdated Inventory\n')
+    for item, quantity in inventory.items():
+        print(f'- {item}: {quantity} in stock')
